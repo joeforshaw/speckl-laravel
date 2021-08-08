4724b948ce7b476db2a5da68df46cbeabe2c312e
Joe Forshaw <joe@joeforshaw.com>
2021-08-08 15:03:47 +0100

    Changed classes

--- src/Config.php
+++ src/Config.php
@@ -1,8 +1,8 @@
 <?php
 
 use Speckl\Config;
-use Speckl\Laravel\ExampleBlock;
+use Speckl\Laravel\GroupBlock;
 use Speckl\Laravel\Scope;
 
-Config::set('exampleBlockClass', ExampleBlock::class);
+Config::set('groupBlockClass', GroupBlock::class);
 Config::set('scopeClass', Scope::class);
