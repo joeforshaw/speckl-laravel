5c2e034719274c5693607eda49ca82c821b27b52
Joe Forshaw <joe@joeforshaw.com>
2021-08-08 15:35:16 +0100

    Replaced groupblock with scope

--- src/Config.php
+++ src/Config.php
@@ -1,8 +1,6 @@
 <?php
 
 use Speckl\Config;
-use Speckl\Laravel\GroupBlock;
 use Speckl\Laravel\Scope;
 
-Config::set('groupBlockClass', GroupBlock::class);
 Config::set('scopeClass', Scope::class);
--- src/Speckl/Laravel/Scope.php
+++ src/Speckl/Laravel/Scope.php
@@ -2,11 +2,23 @@
 
 namespace Speckl\Laravel;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase;
+use Mockery;
 use Speckl\ScopeTrait;
 
 class Scope extends TestCase {
-  use ScopeTrait;
+  use ScopeTrait, RefreshDatabase;
+
+  public function beforeCallback() {
+    Mockery::close();
+    parent::setUp();
+  }
+
+  public function afterCallback() {
+    parent::tearDown();
+  }
 
   public function createApplication() {
     $app = require __DIR__.'/../../../../../../bootstrap/app.php';
